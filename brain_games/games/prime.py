efb21e4d5cd20e1d6ccbcb26b3fb4c8a941238c6
Malafeev Artem <m.a.yevgenyevich@gmail.com>
2020-09-10 09:33:59 +0300

    step9: add new game prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,53 @@
+"""Game logic module."""
+from brain_games.random_number import random_generator
+from brain_games.index import play_game
+
+import math
+
+
+description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    """
+    Сheck if it is a prime number.
+
+    Args:
+        number (int): Random value.
+
+    Returns:
+        bool: Prime or not.
+    """
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+
+    divider = 2
+    limit = int(math.sqrt(number))
+
+    while divider <= limit:
+        if number % divider == 0:
+            return False
+        divider += 1
+
+    return True
+
+
+def get_game_data():
+    """
+    Collect game data.
+
+    Returns:
+        description (str): Game rules.
+        data (list): Сontains a question and an answer.
+    """
+    number = random_generator()
+    question = 'Question: {0}'.format(number)
+    true_answer = 'yes' if is_prime(number) else 'no'
+    game_data = [question, true_answer]
+
+    return game_data
+
+
+play_game(description, get_game_data)
